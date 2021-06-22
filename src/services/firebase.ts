a67a8c520b44edce24b4522a0eb365e457a5500f
vanderL <vnd.vander@gmail.com>
2021-06-22 12:52:35 -0300

    Firebase integration

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBHaxKXYTLik1WUeA8orUuKHI7dpoZWJBw",
-    authDomain: "letmeask-1dff2.firebaseapp.com",
-    databaseURL: "https://letmeask-1dff2-default-rtdb.firebaseio.com",
-    projectId: "letmeask-1dff2",
-    storageBucket: "letmeask-1dff2.appspot.com",
-    messagingSenderId: "79968660140",
-    appId: "1:79968660140:web:3ae934bf51355a1da24d09"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
